f3dbd9ff6001789f3c7dbb19fd7449be78d445be
Davi-Avila <davidomingosavila202@gmail.com>
2025-05-20 22:33:35 -0300

    Update MainFabrica.java

--- src/Fabrica/MainFabrica.java
+++ src/Fabrica/MainFabrica.java
@@ -1,7 +1,31 @@
 package Fabrica;
 
+import java.util.Scanner;
+
 public class MainFabrica {
 	public static void main(String[]args) {
+		Scanner entrada = new Scanner(System.in);
+		Operacoes operacao = new Operacoes();
+		int opcaoMenu = 0;
 		
+		while (opcaoMenu != 5) {
+			operacao.menu();
+			opcaoMenu = entrada.nextInt();
+			switch (opcaoMenu) {
+			case 1:
+				operacao.cadastrar();
+				break;
+			case 2:
+				operacao.RegistrarProducao();
+				break;
+			case 3:
+				operacao.relatorio();
+				break;
+			default: 
+				System.out.println("Opção invalida");
+				break;
+			}	
+		}
+		entrada.close();
 	}
 }
