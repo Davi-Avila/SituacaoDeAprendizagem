77d96554bc7edacea8ef34053a9deb62ada457e2
Davi-Avila <davidomingosavila202@gmail.com>
2025-05-22 22:00:51 -0300

    Update Operacoes.java
    Correção de erros e adição da média e soma por setor

--- src/Fabrica/Operacoes.java
+++ src/Fabrica/Operacoes.java
@@ -59,17 +59,22 @@ public class Operacoes {
 	public void relatorio() {
 				//Faz a soma da produção de cada dia da semana
 		int soma = 0;
+		int media = 0;
 		for (int i = 0;i < 5;i++) {
 			for (int j = 0;j < 4;j++) {
 				soma += semana[j][i];
+				if(semana[j][i] > 0) {
+					media++;
+				}
 			}
 			semana[6][i] = soma;
+			if (media > 0) {
+			semana[7][i] = semana[6][i] / media;
+			}
 			soma = 0;
+			media = 0;
 		}
 		
-		for (int i = 0; i < 5;i++) {
-			semana[7][i] = semana[6][i] / dia;
-		}
 		
 		
 		
@@ -85,6 +90,70 @@ public class Operacoes {
 			}
 			System.out.println();
 		}
+		
+		int somaA = 0; int somaP = 0; int somaM = 0;
+		int mediaA = 0; int mediaP = 0; int mediaM = 0;
+		
+		for (int i = 0;i < setores.size();i++) {
+			switch (setores.get(i)) {
+			case "algodao":
+				somaA += semana[6][i];
+				mediaA++;
+				break;
+			case "poliester":
+				somaP += semana[6][i];
+				mediaP++;
+				break;
+			case "mistos":
+				somaM += semana[6][i];
+				mediaM++;
+				break;
+			}	
+		}
+		if(mediaA == 0) {
+			mediaA = somaA;
+		}else {
+		mediaA = somaA / mediaA;
+		}
+		
+		if(mediaP == 0) {
+			mediaP = somaP;
+		}else {
+		mediaP = somaP / mediaP;;
+		}
+		
+		if(mediaM == 0) {
+			mediaM = somaM;
+		}else {
+		mediaM = somaP / mediaP;
+		}
+		
+		int opcao = 0;
+		while (opcao != 4) {
+			System.out.println("Digite qual setor deseja ver a produção total e sua média"
+							+ "(1- Algodão, 2- Poliéster, 3- Mistos, 4- Sair: ");
+			opcao = entrada.nextInt();
+			switch(opcao) {
+			case 1:
+				System.out.println("Produção total do setor de algodão: " + somaA);
+				System.out.println("Média da produção: " + mediaA);
+				break;
+			case 2:
+				System.out.println("Produção total do setor de algodão: " + somaP);
+				System.out.println("Média da produção: " + mediaP);
+				break;
+			case 3:
+				System.out.println("Produção total do setor de algodão: " + somaM);
+				System.out.println("Média da produção: " + mediaM);
+				break;
+			case 4:
+				break;
+			}
+		}
+		
+		
+		
+		
 		
 		if (semana2[0][0] != 0) {
 			System.out.println("Relatório da semana anterior: ");
